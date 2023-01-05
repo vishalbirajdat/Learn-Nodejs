6a9f075e8afc6acba6dfdd3e519f41b1e85e4db2
vishal Birajdar <65212461+vishalbirajdat@users.noreply.github.com>
2023-01-05 12:08:56 +0530

    Create exercise.js

---
+++ exercise.js
@@ -0,0 +1,20 @@
+console.log("i am a " + job );
+var job = "Web developer";
+console.log("i am a " + job );
+
+function whoAmI() {
+    console.log("i am a " + job );
+var job = "Full Stack";
+console.log("i am a " + job );
+}
+
+whoAmI();
+
+
+
+// output : 
+
+// i am a undefined
+// i am a Web developer
+// i am a undefined
+// i am a Full Stack

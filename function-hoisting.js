e9d9e99abd6b3cbd48829d92ade665708b729549
vishal Birajdar <65212461+vishalbirajdat@users.noreply.github.com>
2023-01-05 11:50:23 +0530

    Create function-hoisting.js

---
+++ function-hoisting.js
@@ -0,0 +1,16 @@
+sum(10, 2);
+
+function sum(a, b) {
+    let add = a+b;
+    console.log(add);
+}
+
+
+// not run on 
+
+sum(10, 2);
+
+const sum = (a, b)=>{
+    let add = a+b;
+    console.log(add);
+}

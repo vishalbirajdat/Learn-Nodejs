2f213d7ff8dad59a333681941eb88ce4c97c7e3f
vishal Birajdar <65212461+vishalbirajdat@users.noreply.github.com>
2023-01-05 17:01:24 +0530

    Add files via upload

---
+++ Es6/Array-object_Destructuring.js
@@ -0,0 +1,15 @@
+var arr = ["Hello", "World"]  
+  
+// destructuring assignment  
+var [first, second] = arr;  
+  
+console.log(first); // Hello  
+console.log(second); // World
+
+// object destructuring
+
+const num = {x: 100, y: 200};  
+const {x, y} = num;  
+  
+console.log(x); // 100  
+console.log(y); // 200  
---
+++ Es6/es6_all.js
@@ -0,0 +1,39 @@
+
+// ES5
+var x = function(x, y) {
+    return x * y;
+ }
+ 
+ // ES6
+ const x = (x, y) => x * y;
+
+
+
+//  The let keyword
+//  The const keyword
+//   Arrow Functions
+// The ... Operator
+// For/of
+// Map Objects
+// Set Objects
+// Classes
+// Promises
+// Symbol
+// Default Parameters
+// Function Rest Parameter
+// String.includes()
+// String.startsWith()
+// String.endsWith()
+// Array.from()
+// Array keys()
+// Array find()
+// Array findIndex()
+// New Math Methods
+// New Number Properties
+// New Number Methods
+// New Global Methods
+// Object entries
+// JavaScript Modules
+
+
+
